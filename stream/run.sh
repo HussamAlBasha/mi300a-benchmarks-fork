9cdb7374f17c462bbf04e27ba2b111cc407fb38e
Jacob Wahlgren <jacobwah@kth.se>
2025-01-09 05:48:07 -0800

    stream numactl

--- stream/run.sh
+++ stream/run.sh
@@ -1,5 +1,6 @@
 for m in malloc hipMalloc hipHostMalloc hipMallocManaged; do
-	for n in 1 2 4 8 16 32 64 128; do
-		OMP_NUM_THREADS=$n ./mstream $m
+	#for n in 1 2 4 8 16 32 64 128; do
+	for n in 1 3 6 9 12 15 18 21 24; do
+		OMP_PROC_BIND=true OMP_NUM_THREADS=$n ./mstream $m
 	done > $m.log
 done
