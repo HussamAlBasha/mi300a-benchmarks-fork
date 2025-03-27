7906f3c6153857f68ba96faad3312c341091c021
Jacob Wahlgren <jacobwah@kth.se>
2025-03-27 05:35:46 -0700

    histo: add validation suite

---
+++ atomics/test_histo.py
@@ -0,0 +1,48 @@
+import pytest
+import subprocess
+import os
+
+def run(args):
+    env = os.environ.copy()
+    env["HSA_XNACK"] = "1"
+    return subprocess.run(["./histo"] + args, timeout=60, check=True, env=env)
+
+def test_no_cpu_or_gpu():
+    with pytest.raises(subprocess.CalledProcessError):
+        run([])
+
+def test_1_cpu():
+    run(["-c", "1"])
+
+def test_multi_cpu():
+    run(["-c", "24"])
+
+def test_1_gpu():
+    run(["-g", "1"])
+
+def test_multi_gpu():
+    run(["-g", "1024"])
+
+def test_tpb_gpu():
+    run(["-g", "1024", "-b", "64"])
+
+def test_1_cpu_gpu():
+    run(["-c", "1", "-g", "1"])
+
+def test_multi_cpu_gpu():
+    run(["-c", "24", "-g", "1024"])
+
+def test_u64():
+    run(["-c", "1", "-g", "1", "-d", "u64"])
+
+def test_f32():
+    run(["-c", "1", "-g", "1", "-d", "f32"])
+
+def test_f64():
+    run(["-c", "1", "-g", "1", "-d", "f64"])
+
+def test_gpu_no_system():
+    run(["-g", "1024", "-S"])
+
+def test_cpu_gpu_no_system():
+    run(["-c", "24", "-g", "1024", "-S"])
