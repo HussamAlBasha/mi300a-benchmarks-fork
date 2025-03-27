eb41997e3a842e8af31698aac1ca5580531acfc5
Jacob Wahlgren <jacobwah@kth.se>
2025-03-27 05:52:35 -0700

    histo: add hipMalloc option

--- atomics/test_histo.py
+++ atomics/test_histo.py
@@ -2,9 +2,9 @@ import pytest
 import subprocess
 import os
 
-def run(args):
+def run(args, xnack=1):
     env = os.environ.copy()
-    env["HSA_XNACK"] = "1"
+    env["HSA_XNACK"] = str(xnack)
     return subprocess.run(["./histo"] + args, timeout=60, check=True, env=env)
 
 def test_no_cpu_or_gpu():
@@ -46,3 +46,6 @@ def test_gpu_no_system():
 
 def test_cpu_gpu_no_system():
     run(["-c", "24", "-g", "1024", "-S"])
+
+def test_hip_malloc():
+    run(["-c", "1", "-g", "1", "-H"], xnack=0)
